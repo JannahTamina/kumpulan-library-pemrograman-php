6633790bb226117fbb7cab7f4ffd74832c9af1e6
JannahTamina <107163427+JannahTamina@users.noreply.github.com>
2022-06-11 11:24:22 +0700

    Update index.php

--- index.php
+++ index.php
@@ -15,6 +15,15 @@ function tambah ()
   $x = 5;
   $y = 4;
   echo $x + $y;
+  
+     /*
+Pengurangan di php 7 dikurang 4 hasil nya? 
+*/
+$x = 7; // variable a diisi dengan nilai 7
+$y = 4; // variable b di isi dengan nilai 4
+$z = $x - $y; // proses 
+echo "Hasil dari Pengurangan $x - $y = $z"; // hasil outputnya
+
 }
 ?>
 
